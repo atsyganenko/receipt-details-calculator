a986fccfeb0fa44f194eb0c11d88bd8a83a78e6f
a.tsyganenko <a.tsyganenko@intive.com>
2019-01-15 22:40:39 +0100

    test cases added to ProductTest

--- src/main/test/com/atsyg/receipt_details_calculator/products/ProductTest.java
+++ src/main/test/com/atsyg/receipt_details_calculator/products/ProductTest.java
@@ -1,6 +1,9 @@
 package com.atsyg.receipt_details_calculator.products;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 
@@ -10,28 +13,23 @@ class ProductTest {
 
     private static final String PRODUCT_NAME = "test product";
 
-    @Test
-    void getPriceIncludingTaxes_returnsPriceWithTaxesApplied() {
-        Product product = new Product(PRODUCT_NAME, "10");
+    @DisplayName("Calculate final price (including taxes)")
+    @ParameterizedTest(name = "Final price for the product with price={0} should be {1}")
+    @CsvSource({"10, 11.75", "29.49, 34.65", "2.37, 2.80", "1, 1.20", "0.00, 0.00"})
+    void getPriceIncludingTaxes_returnsPriceWithTaxesApplied(String basePrice, String finalPrice) {
+        Product product = new Product(PRODUCT_NAME, basePrice);
 
-        BigDecimal expected = new BigDecimal(11.75);
+        BigDecimal expected = new BigDecimal(finalPrice);
         assertEquals(expected, product.getPriceIncludingTaxes());
     }
 
-    @Test
-    // rounds up to the nearest 0.05
-    void getPriceIncludingTaxes_roundsUpPriceAccordingToPolicy() {
-        Product product = new Product(PRODUCT_NAME, "29.49");
+    @DisplayName("Calculate taxes")
+    @ParameterizedTest(name = "Taxes applied to the product with price={0} should be {1}")
+    @CsvSource({"10, 1.75", "29.49, 5.16", "2.37, 0.43", "1, 0.20", "0.00, 0.00"})
+    void getTaxes_returnsAmountOfTaxesApplied(String basePrice, String taxes) {
+        Product product = new Product(PRODUCT_NAME, basePrice);
 
-        BigDecimal expected = new BigDecimal("34.65");
-        assertEquals(expected, product.getPriceIncludingTaxes());
-    }
-
-    @Test
-    void getTaxes_returnsAmountOfTaxesApplied() {
-        Product product = new Product(PRODUCT_NAME, "10");
-
-        BigDecimal expected = new BigDecimal(1.75);
+        BigDecimal expected = new BigDecimal(taxes);
         assertEquals(expected, product.getTaxes());
     }
 
